d7b4ac9fc1bdd3c8cdb050d107c20bb0fa3637c7
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-05-01 16:02:23 +0300

    Fix the fake use happening every tick if the last item from hand was consumed

--- src/main/java/fi/dy/masa/placementpreview/event/TickHandler.java
+++ src/main/java/fi/dy/masa/placementpreview/event/TickHandler.java
@@ -308,6 +308,9 @@ public class TickHandler
             fakePlayer.setLocationAndAngles(realPlayer.posX, realPlayer.posY, realPlayer.posZ, realPlayer.rotationYaw, realPlayer.rotationPitch);
 
             ItemStack stackCopy = stack.copy();
+            // A second copy, to which to reset after the fake use action has been completed.
+            // This prevents the fake use from happening again every tick if the only item from the fake player's hand was consumed.
+            ItemStack stackCopy2 = stack.copy();
             fakePlayer.setHeldItem(hand, stackCopy);
 
             try
@@ -315,18 +318,21 @@ public class TickHandler
                 EnumActionResult result = stackCopy.getItem().onItemUseFirst(stackCopy, fakePlayer, fakeWorld, posCenter, side, hitX, hitY, hitZ, hand);
                 if (result == EnumActionResult.SUCCESS)
                 {
+                    fakePlayer.setHeldItem(hand, stackCopy2);
                     return result;
                 }
 
                 result = stackCopy.onItemUse(fakePlayer, fakeWorld, posCenter, hand, side, hitX, hitY, hitZ);
                 if (result == EnumActionResult.SUCCESS)
                 {
+                    fakePlayer.setHeldItem(hand, stackCopy2);
                     return result;
                 }
 
                 result = stackCopy.useItemRightClick(fakeWorld, fakePlayer, hand).getType();
                 if (result == EnumActionResult.SUCCESS)
                 {
+                    fakePlayer.setHeldItem(hand, stackCopy2);
                     return result;
                 }
             }
@@ -343,6 +349,7 @@ public class TickHandler
                         " blacklisting it for this session\n", regName);
                 }
 
+                fakePlayer.setHeldItem(hand, stackCopy2);
                 this.blacklistedItems.add(regName);
                 this.whitelistedItems.remove(regName);
             }
